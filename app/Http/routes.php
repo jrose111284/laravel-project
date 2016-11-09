231784b491375328aabfd505326527dffecd9b47
jamie rose <jrose111284@gmail.com>
2016-11-09 22:38:54 +0000

    crud with database

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,17 +11,47 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/insert', function (){
+//
+//    DB::insert('insert into posts(title, content) values(?, ?)', ['php with laravel', 'php is the best']);
+//
+//});
 
-Route::get('/contact', 'PostsController@contact');
+//data base raw enquires
+
+//Route::get('/insert',  function (){
+//   $results = DB::select('select * from posts where id=?', [1]);
+//
+//    foreach ($results as $result) {
+//        return $result->content;
+//    }
+//});
+
+//Route::get('/update', function (){
+//
+//    $updated = DB::update('update posts set title = "update title" where id =?', [1]);
+//
+//    return $updated;
+//});
+
+//Route::get('/delete', function (){
+//    $deleted = DB::delete('delete from posts where id = ?', [1]);
+//
+//    return $deleted;
+//});
+
+//Route::get('/', function () {
+//
+//    return view('welcome');
+//});
+
+//Route::get('/contact', 'PostsController@contact');
 
 
 //Route::get('/post/{id}', 'PostsController@index');
 //
 //Route::resource('post', 'PostsController');
 
-Route::get('post/{id}', 'PostsController@showposts');
+//Route::get('post/{id}', 'PostsController@showposts');
 
     
